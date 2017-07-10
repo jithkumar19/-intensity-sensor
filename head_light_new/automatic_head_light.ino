9fe8c6d29349cee98c6c93417a367dd87ac94c7d
Ajith Kumar <jithkumar19@gmail.com>
2017-07-10 12:16:48 +0530

    Rename head_light_new.ino to automatic_head_light.ino
    An automatic two wheeler head light system.

---
+++ head_light_new/automatic_head_light.ino
@@ -0,0 +1,46 @@
+#include <Wire.h>
+
+#include <Wire.h>
+#include <BH1750.h>
+
+
+BH1750 lightMeter;
+int led1 = 12;
+int led2 = 13;
+
+
+void setup()
+{
+  Serial.begin(9600);
+  lightMeter.begin();
+  Serial.println("Running...");
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+}
+
+
+void loop()
+ {
+  uint16_t lux = lightMeter.readLightLevel();
+  Serial.print("Light: ");
+  Serial.print(lux);
+  Serial.println(" lx");
+  delay(1000);
+ if (lux >= 1000)
+{
+ digitalWrite(led1, HIGH);   // turn the LED on (HIGH is the voltage level)
+  }
+ else (lux <= 1000);
+{
+ digitalWrite(led1, LOW);
+}
+ 
+if(lux >= 1000)
+{
+digitalWrite(led2, HIGH);
+}
+else
+{
+digitalWrite(led2, LOW);
+}
+}
